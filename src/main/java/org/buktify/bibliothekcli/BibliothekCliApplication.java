153f76c0dd1f71e5be5f69761cc73a0d0f7dc379
crazy <matweq.max2015@yandex.ru>
2023-03-27 16:20:26 +0300

    Initial commit

---
+++ src/main/java/org/buktify/bibliothekcli/BibliothekCliApplication.java
@@ -0,0 +1,13 @@
+package org.buktify.bibliothekcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothekCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothekCliApplication.class, args);
+    }
+
+}
